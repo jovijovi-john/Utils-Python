237c25c5a60a13909d753f2e08f5e11dc06983f5
John <jovijovi2019@hotmail.com>
2022-10-31 18:45:31 -0300

    validateInt

---
+++ validateInt.py
@@ -0,0 +1,18 @@
+from isInteger import isInteger
+from printMessageError import printMessageError
+
+def validateInt(msg="Digite um número: ", msgError="Digite um número válido"):
+  """
+    Faz um loop que só para quando o usuário informar um inteiro válido
+
+    :param str msg:       Mensagem mostrada no input
+    :param str msgError:  Mensagem mostrada quando o usuário informa um número inválido
+  """
+  number = input(msg)    
+  
+  if (isInteger(number)):
+    return int(number)
+  else:
+    printMessageError(msgError)
+    return validateInt(msg, msgError)
+
