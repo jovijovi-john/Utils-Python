214d032e321cb5689e7f4d8f618a50515d417827
John <jovijovi2019@hotmail.com>
2022-10-31 18:46:10 -0300

    removing pycache and intValidator_1.0

--- intValidator.py
+++
@@ -1,8 +0,0 @@
-def validateInt(msg="Digite um número: ", msgError="Digite um número válido"):
-  try:
-    number = input(msg)
-    number = number.strip() # removendo os espaços em brancos
-    number = int(number) # convertendo pra inteiro
-  except ValueError:
-    print(f"\033[1;31m{msgError}\033[m")
-    validateInt(msg, msgError)
